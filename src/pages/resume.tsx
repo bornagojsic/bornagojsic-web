40afecbd43d939c3d1f2684ceda92b44ada04e52
bornagojsic <69301624+bornagojsic@users.noreply.github.com>
2023-05-23 00:33:00 +0200

    Added title and description to Resume Page

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,9 +1,13 @@
 import {FC, memo} from 'react';
-
+import Page from '../components/Layout/Page';
 import PdfPage from '../components/ResumePage';
+import {homePageMeta} from '../data/data';
 
 const Resume: FC = memo(() => {
-  return <PdfPage />;
+  const {title, description} = homePageMeta;
+  return <Page description={description} title={title}>
+    <PdfPage/>;
+  </Page>
 });
 
 export default Resume;
