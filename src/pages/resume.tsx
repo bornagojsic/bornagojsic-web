0f42b6b3aa9269a192ae1c6d91547f6f2ad821cd
bornagojsic <69301624+bornagojsic@users.noreply.github.com>
2023-05-24 00:53:11 +0200

    Refactored imports

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,8 +1,8 @@
 import {FC, memo} from 'react';
 
+import {homePageMeta} from '../data/data';
 import PdfPage from '../components/ResumePage';
 import Page from '../components/Layout/Page';
-import {homePageMeta} from '../data/data';
 
 const Resume: FC = memo(() => {
   const {title, description} = homePageMeta;
